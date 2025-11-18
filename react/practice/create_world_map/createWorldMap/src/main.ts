cb614f39c11fb3ca73534d8daf5328f80f1b2a90
pengom0809 <pengom0809@gmail.com>
2025-11-18 16:30:56 +0900

    chore : [TIL_Front] Vue 프로젝트 초기 설정 완료

--- react/practice/create_world_map/createWorldMap/src/main.ts
+++ react/practice/create_world_map/createWorldMap/src/main.ts
@@ -3,10 +3,22 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+
+//-------------PrimeVue 필수 스타일 가져오기----------------------//
+import 'primevue/resources/themes/aura-light-green/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css'; 
 
 const app = createApp(App)
+const pinia = createPinia(); //Pinia 인스타 생성
 
-app.use(createPinia())
+//--------PrimeVue 플로그인 등록 -----//
+app.use(PrimeVue, {
+    ripple: true
+});
+
 app.use(router)
+app.use(pinia);
 
 app.mount('#app')
